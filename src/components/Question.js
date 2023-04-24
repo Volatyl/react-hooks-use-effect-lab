f4ea17cd46d9ecda2169fd0ed6617a9ffa5644b1
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-24 11:28:41 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    let intervalId = setInterval(
+      () => setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1),
+      1000
+    );
+
+    const timeOut = setTimeout(() => {
+      clearInterval(intervalId);
+      setTimeRemaining(10);
+      onAnswered(false);
+    }, 10000);
+
+    return function () {
+      clearInterval(intervalId);
+      clearTimeout(timeOut);
+    };
+  }, [onAnswered]);
+  console.log(timeRemaining);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
